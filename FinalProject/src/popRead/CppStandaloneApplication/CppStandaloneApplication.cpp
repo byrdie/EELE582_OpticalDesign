be4567c2f6885dd6965554f67cafcd40794ec8fc
byrdie <roytsmart@gmail.com>
2017-04-26 20:31:08 -0600

    Done with the presentation

---
+++ FinalProject/src/popRead/CppStandaloneApplication/CppStandaloneApplication.cpp
@@ -0,0 +1,142 @@
+// CppStandaloneApplication.cpp : Defines the entry point for the console application.
+//
+
+
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <functional>
+#include <assert.h>
+#include <vector>
+
+#import "C:\\Program Files\\Zemax OpticStudio\\ZOS-API\\Libraries\\ZOSAPII.tlb"
+#import "C:\\Program Files\\Zemax OpticStudio\\ZOS-API\\Libraries\\ZOSAPI.tlb"
+// Note - .tlh files will be generated from the .tlb files (above) once the project is compiled.
+// Visual Studio will incorrectly continue to report IntelliSense error messages however until it is restarted.
+
+
+
+using namespace std;
+using namespace ZOSAPI;
+using namespace ZOSAPI_Interfaces;
+
+double nth_radius(int n, double focal_len, double wavlen);
+int zone_ind(double radius, double focal_len, double wavlen);
+void handleError(std::string msg);
+void logInfo(std::string msg);
+void finishStandaloneApplication(IZOSAPI_ApplicationPtr TheApplication);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	CoInitialize(NULL);
+
+	// Create the initial connection class
+	IZOSAPI_ConnectionPtr TheConnection(__uuidof(ZOSAPI_Connection));
+
+
+	// Attempt to create a Standalone connection
+	IZOSAPI_ApplicationPtr TheApplication = TheConnection->CreateNewApplication();
+	if (TheApplication == nullptr)
+	{
+		handleError("An unknown error occurred!");
+		return -1;
+	}
+
+	// Check the connection status
+	if (!TheApplication->IsValidLicenseForAPI)
+	{
+		handleError("License check failed!");
+		return -1;
+	}
+	if (TheApplication->Mode != ZOSAPI_Mode::ZOSAPI_Mode_Server)
+	{
+		handleError("Standlone application was started in the incorrect mode!");
+		return -1;
+	}
+
+	// Start the Zemax application
+	IOpticalSystemPtr TheSystem = TheApplication->PrimarySystem;
+
+	// Define parameters of the optical system
+	double focal_len = 1373;	// Focal length of the zone plate in mm.
+
+
+	// Open a new Zemax file
+	TheSystem->LoadFile(_bstr_t::_bstr_t("E:\\Users\\byrdie\\School\\Classes\\EELE582_OpticalDesign\\FinalProject\\zemax\\final1.zmx"),false);
+
+
+	I_AnalysesPtr analyses = TheSystem->Analyses;
+
+	IA_Ptr psf = analyses->Get_AnalysisAtIndex(1);
+
+	IAR_Ptr psf_results = psf->GetResults();
+	IAR_DataGridPtr psf_data = psf_results->GetDataGrid(0);
+	SAFEARRAY * psf_values = psf_data->Values;
+	void * pv;
+	SafeArrayAccessData(psf_values, &pv);
+	double * psf_double = reinterpret_cast<double *>(pv);
+
+	FILE * psf_file = fopen("E:\\Users\\byrdie\\School\\Classes\\EELE582_OpticalDesign\\FinalProject\\psf.bin", "wb");
+	fwrite(psf_double, sizeof(double), psf_data->Nx * psf_data->Ny, psf_file);
+	fclose(psf_file);
+
+	FILE * psf_meta = fopen("E:\\Users\\byrdie\\School\\Classes\\EELE582_OpticalDesign\\FinalProject\\meta.bin", "wb");
+	int nx = psf_data->Nx;
+	int ny = psf_data->Ny;
+	double min_x = psf_data->MinX;
+	double min_y = psf_data->MinY;
+	double dx = psf_data->Dx;
+	double dy = psf_data->Dy;
+	fwrite(&focal_len, sizeof(double), 1, psf_meta);
+	fwrite(&nx, sizeof(int), 1, psf_meta);
+	fwrite(&ny, sizeof(int), 1, psf_meta);
+	fwrite(&min_x, sizeof(double), 1, psf_meta);
+	fwrite(&min_y, sizeof(double), 1, psf_meta);
+	fwrite(&dx, sizeof(double), 1, psf_meta);
+	fwrite(&dy, sizeof(double), 1, psf_meta);
+	fclose(psf_meta); 
+
+
+	TheSystem->Close(false);
+			
+	// Clean up
+	finishStandaloneApplication(TheApplication);
+	
+
+	return 0;
+}
+
+double nth_radius(int n, double focal_len, double wavlen) {
+
+	return sqrt((n * wavlen * focal_len) + (0.25 * n * n * wavlen * wavlen));
+
+}
+
+int zone_ind(double radius, double focal_len, double wavlen) {
+	return floor(2 * (sqrt(focal_len * focal_len + radius * radius) - focal_len) / wavlen);
+}
+
+void handleError(std::string msg)
+{
+	throw new exception(msg.c_str());
+}
+
+void logInfo(std::string msg)
+{
+	printf("%s", msg.c_str());
+}
+
+void finishStandaloneApplication(IZOSAPI_ApplicationPtr TheApplication)
+{
+    // Note - TheApplication will close automatically when this application exits, so this isn't strictly necessary in most cases
+	if (TheApplication != nullptr)
+	{
+		TheApplication->CloseApplication();
+	}
+}
+
+
